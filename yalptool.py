0d4770fb28d15d853b0c73f9bd332672310c3d6c
seahawk1986 <seahawk1986@hotmail.com>
2013-10-25 10:44:32 +0200

    update yalptool.py
    Add config file, prepare for python3

--- yalptool.py
+++ yalptool.py
@@ -1,37 +1,13 @@
 #!/usr/bin/python
-
-from_user_name = "yavdr"
-from_series_name = "precise"
-from_ppa_name = "unstable-vdr"
-
-to_user_name = from_user_name
-to_series_name = from_series_name
-to_ppa_name = from_ppa_name
-
-# use False for copy to different PPA
-increment_version = True
-# expect: <upstream-version>-<package-number><build_number_prefix><build-number>~<series>
-# example: 2.0.3-3yavdr0~precise
-build_number_prefix = "yavdr"
-changelog_message = "automatic rebuild"
-
-debemail = "release@yavdr.org"
-debfullname = "yavdr package builder"
-gpgkey = "5289F541"
-
-# if not empty process only packages in include_packages
-include_packages = []
-# and never packages in exclude_packages
-exclude_packages = ["vdr"]
-
-
+import ConfigParser
+import argparse
+import subprocess
 import os
 import shutil
 import sys
+from __future__ import print_function
 from signal import signal, SIGPIPE, SIG_DFL
-from subprocess import call
 from launchpadlib.launchpad import Launchpad
-
 
 def get_subdirs(dir):
   return [name for name in os.listdir(dir) if os.path.isdir(name)]
@@ -42,107 +18,191 @@ def find_ppa(ppas, name):
       return ppa
   return None
 
-  
-cachedir = os.path.expanduser("~/.launchpadlib/cache/")
-cwd = os.getcwd()
+def move_packages(config):
+    cachedir = os.path.join(os.path.expanduser("~"), ".launchpadlib", "cache")
+    cwd = os.getcwd()
 
-print "connecting to Launchpad..."
-launchpad = Launchpad.login_anonymously('yalptool', 'production', cachedir)
-ubuntu = launchpad.distributions["ubuntu"]
-from_series = ubuntu.getSeries(name_or_version=from_series_name)
-to_series = ubuntu.getSeries(name_or_version=to_series_name)
+    print("connecting to Launchpad...")
+    launchpad = Launchpad.login_anonymously('yalptool', 'production', cachedir)
+    ubuntu = launchpad.distributions["ubuntu"]
+    from_series = ubuntu.getSeries(name_or_version=config.from_series_name)
+    to_series = ubuntu.getSeries(name_or_version=config.to_series_name)
 
-os.putenv("DEBEMAIL", debemail)
-os.putenv("DEBFULLNAME", debfullname)
-os.putenv("GPGKEY", gpgkey)
-changelog = os.path.join("debian", "changelog")
+    changelog = os.path.join("debian", "changelog")
 
-from_user = launchpad.people[from_user_name]
-if from_user is None:
-  print >>sys.stderr, "can't find 'from' user " + from_user_name
-  exit(1)
+    from_user = launchpad.people[config.from_user_name]
+    if not from_user:
+      exit("can't find 'from' user {0}".format(config.from_user_name))
 
-from_ppas = from_user.ppas
-from_ppa = find_ppa(from_ppas, from_ppa_name)
-if from_ppa is None:
-  print >>sys.stderr, "can't find 'from' ppa " + from_ppa_name
-  exit(1)
+    from_ppas = from_user.ppas
+    from_ppa = find_ppa(from_ppas, config.from_ppa_name)
+    if not from_ppa:
+      exit("can't find 'from' ppa {0}".format(config.from_ppa_name))
 
-to_user = launchpad.people[to_user_name]
-if to_user is None:
-  print >>sys.stderr, "can't find 'to' user " + to_user_name
-  exit(1)
+    to_user = launchpad.people[config.to_user_name]
+    if not to_user:
+      exit("can't find 'to' user {0}".format(config.to_user_name))
 
-to_ppas = to_user.ppas
-to_ppa = find_ppa(to_ppas, to_ppa_name)
-if to_ppa is None:
-  print >>sys.stderr, "can't find 'to' ppa " + to_ppa_name
-  exit(1)
+    to_ppas = to_user.ppas
+    to_ppa = find_ppa(to_ppas, config.to_ppa_name)
+    if not to_ppa:
+      exit("can't find 'to' ppa {0}".format(config.to_ppa_name))
 
-sources = from_ppa.getPublishedSources(distro_series=from_series, status="Published")
-for s in sources:
-  source_name = s.source_package_name
-  source_version = s.source_package_version
-  if (     (not include_packages or source_name in include_packages)
-       and (not exclude_packages or not source_name in exclude_packages)):
-    print "=========="
-    print "source_package_name: " + source_name
-    print "source_package_version: " + source_version
-    urls = s.sourceFileUrls()
-    for u in urls:
-      if u.endswith(".dsc"):
-        dir = source_name
-        if os.access(dir, os.F_OK):
-          print >>sys.stderr, dir + " already exists, skipping " + source_name
-          break
-
-        new_version = source_version
-        if increment_version:
-          pos = source_version.rindex("-")
-          if pos < 0:
-            print >>sys.stderr, source_version + " has not the expected version number scheme, skipping " + source_name
-            break
-
-          pos2 = source_version.find(build_number_prefix, pos)
-          if pos2 < 0:
-            print >>sys.stderr, source_version + " has not the expected version number scheme, skipping " + source_name
-            break
-
-          pos2 = pos2 + len(build_number_prefix)
-          pos3 = pos2 + 1
-          while pos3 < len(source_version):
-            if not source_version[pos2:pos3].isdigit():
-              pos3 = pos3 - 1
+    sources = from_ppa.getPublishedSources(distro_series=from_series,
+                                                             status="Published")
+    for s in sources:
+      source_name = s.source_package_name
+      source_version = s.source_package_version
+      if ((not config.include_packages or source_name in config.include_packages)
+          and (not config.exclude_packages or not source_name in config.exclude_packages)):
+        print("==========\n"
+              "source_package_name:", source_name, "\n"
+              "source_package_version:", source_version, "\n")
+        urls = s.sourceFileUrls()
+        for u in urls:
+          if u.endswith(".dsc"):
+            dir = source_name
+            if os.access(dir, os.F_OK):
+              print("{0} already exists, skipping {1}".format(dir, source_name),
+                    file=sys.stderr)
               break
-            pos3 = pos3 + 1
-          if not source_version[pos2:pos3].isdigit():
-            print >>sys.stderr, source_version + " has not the expected version number scheme, skipping " + source_name
-            break
 
-          build_number = str(int(source_version[pos2:pos3]) + 1)
-          new_version = source_version[:pos2] + build_number
-          if source_version.endswith("~" + from_series_name):
-            new_version = new_version + "~" + to_series_name
-          print "new source_package_version: " + new_version
+            new_version = source_version
+            if config.increment_version:
+              pos = source_version.rindex("-")
+              if pos < 0:
+                print("{0} has not the expected version number scheme, skipping {1}".format(
+                      source_version,source_name), file=sys.stderr)
+                break
 
-        os.mkdir(dir)
-        os.chdir(dir)
-        print "dget -xuq " + u
-        call("dget -xuq " + u, shell = True)
-        subdirs = get_subdirs(".")
-        for subdir in subdirs:
-          os.chdir(subdir)
-          if os.access(changelog, os.F_OK | os.W_OK) and os.path.isfile(changelog):
-            print "dch --newversion " + new_version + " -u medium --distribution " + to_series_name +" --force-distribution \"" + changelog_message + "\""
-            call("dch --newversion " + new_version + " -u medium --distribution " + to_series_name +" --force-distribution \"" + changelog_message + "\"", shell = True)
-            print "debuild -S -sa -k$GPGKEY"
-            call("debuild -S -sa -k$GPGKEY", shell = True, preexec_fn = lambda: signal(SIGPIPE, SIG_DFL))
-          os.chdir(os.path.join(cwd, dir))
-          changes_file = source_name + "_" + new_version + "_source.changes"
-          if not os.access(changes_file, os.F_OK | os.R_OK) or not os.path.isfile(changes_file):
-            print >>sys.stderr, "can't find " + changes_file + " for upload"
-          else:
-            print "dput -U ppa:" + to_user_name + "/" + to_ppa_name + " " + changes_file
-            call("dput -U ppa:" + to_user_name + "/" + to_ppa_name + " " + changes_file, shell = True)
-        os.chdir(cwd)
-        shutil.rmtree(dir)
+              pos2 = source_version.find(config.build_number_prefix, pos)
+              if pos2 < 0:
+                print("{0} has not the expected version number scheme, skipping {1}".format(
+                      source_version, source_name), file=sys.stderr)
+                break
+
+              pos2 = pos2 + len(config.build_number_prefix)
+              pos3 = pos2 + 1
+              while pos3 < len(source_version):
+                if not source_version[pos2:pos3].isdigit():
+                  pos3 = pos3 - 1
+                  break
+                pos3 = pos3 + 1
+              if not source_version[pos2:pos3].isdigit():
+                print("{0} has not the expected version number scheme, skipping {1}".format(
+                      source_version, source_name), file=sys.stderr)
+                break
+
+              build_number = str(int(source_version[pos2:pos3]) + 1)
+              new_version = source_version[:pos2] + build_number
+              if source_version.endswith("~" + config.from_series_name):
+                new_version = new_version + "~" + config.to_series_name
+              print("new source_package_version:", new_version)
+
+            os.mkdir(dir)
+            os.chdir(dir)
+            print("dget -xuq {0}".format(u))
+            subprocess.call(["dget", "-xuq", u], env=os.environ)
+            subdirs = get_subdirs(".")
+            for subdir in subdirs:
+              os.chdir(subdir)
+              if (os.access(changelog, os.F_OK | os.W_OK) and 
+                                                     os.path.isfile(changelog)):
+                print(("dch --newversion {0} -u medium --distribution {1}"
+                       "--force-distribution \"{2}\"").format(new_version,
+                                                              config.to_series_name,
+                                                              config.changelog_message)
+                )
+                subprocess.call(["dch", "--newversion", new_version,
+                                "-u", "medium",
+                                "--distribution", config.to_series_name,
+                                "--force-distribution", config.changelog_message],
+                                env=os.environ)
+                print("debuild -S -sa -k{0}".format(os.environ["GPGKEY"]))
+                subprocess.call(["debuild", "-S", "-sa", "-k{0}".format(
+                                                         os.environ["GPGKEY"])],
+                                preexec_fn = lambda: signal(SIGPIPE, SIG_DFL),
+                                env=os.environ)
+              os.chdir(os.path.join(cwd, dir))
+              changes_file = "{0}_{1}_source.changes".format(
+                                                       source_name, new_version)
+              if (not os.access(changes_file, os.F_OK | os.R_OK) or 
+                  not os.path.isfile(changes_file)):
+                print("can't find {0} for upload".format(changes_file),
+                      file=sys.stderr)
+              else:
+                print("dput -U ppa: {0}/{1} {2}".format(config.to_user_name,
+                                                        config.to_ppa_name,
+                                                        changes_file))
+                subprocess.call(["dput", "-U", "ppa:{0}/{1}".format(
+                                                            config.to_user_name,
+                                                            config.to_ppa_name),
+                                changes_file], env=os.environ)
+            os.chdir(cwd)
+            shutil.rmtree(dir)
+
+
+class Config:
+    def __init__(self):
+        argparser = argparse.ArgumentParser(
+                                      description='Copy packages in Launchpad.')
+        argparser.add_argument('-c', '--config', metavar='CONFIG',
+                               dest='config', action='append', default=None, 
+                               help='config file(s)')
+        args = vars(argparser.parse_args())
+        self.configparser = ConfigParser.SafeConfigParser()
+        self.configparser.read(args["config"])
+        self.get_config()
+
+    def get_setting(category, setting, default=None):
+        if self.configparser.has_option(category, setting):
+            return self.configparser.get(category, setting)
+        else:
+            return default
+
+    def get_settingb(self, category, setting, default=False):
+        if self.configparser.has_option(category, setting):
+            return self.configparser.getboolean(category, setting)
+        else:
+            return default
+        
+    def get_config(self):
+        # Launchpad Options
+        self.from_user_name = self.get_setting("Launchpad", "from_user_name")
+        self.from_series_name = self.get_setting("Launchpad", "from_series_name")
+        self.from_ppa_name = self.get_setting("Launchpad", "from_ppa_name")
+        self.to_user_name = self.get_setting("Launchpad", "to_user_name",
+                                                            self.from_user_name)
+        self.to_series_name = self.get_setting("Launchpad", "to_series_name",
+                                                          self.from_series_name)
+        self.to_ppa_name = self.get_setting("Launchpad", "to_ppa_name",
+                                                             self.from_ppa_name)
+        # Package Options
+        self.increment_version = self.get_settingb("Options", "increment_version")
+        self.build_number_prefix = self.get_setting("Options", "build_number_prefix",
+                                               "ubuntu")
+        self.changelog_message = self.get_setting("Options", "changelog_message",
+                                               "automatic rebuild")
+        # if not empty process only packages in include_packages
+        self.include_packages = self.get_setting("Options", "include_packages",
+                                                "").split()
+        self.exclude_packages = self.get_setting("Options", "exclude_packages",
+                                                "").split()
+        # Maintainer
+        self.debemail = self.get_setting("Maintainer", "debemail")
+        self.debfullname = self.get_setting("Maintainer", "debfullname")
+        self.gpgkey = self.get_setting("Maintainer", "gpgkey")
+        
+        # set os.environ Variables
+        os.environ["DEBEMAIL"] =  self.debemail
+        os.environ["DEBFULLNAME"] = self.debfullname
+        os.environ["GPGKEY"] = self.gpgkey
+
+
+def main():
+    config = Config()
+    move_packages(config)
+
+        
+if __name__ == '__main__':
+    main()
