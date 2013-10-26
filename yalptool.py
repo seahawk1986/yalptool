73b44f91189d462f4ca92b362141777078dd40d0
Lars Hanisch <dvb@flensrocker.de>
2013-10-26 10:24:33 +0200

    add some error handling

--- yalptool.py
+++ yalptool.py
@@ -2,15 +2,19 @@
 from __future__ import print_function
 import ConfigParser
 import argparse
+import hashlib
 import subprocess
 import os
 import shutil
 import sys
 from signal import signal, SIGPIPE, SIG_DFL
 from launchpadlib.launchpad import Launchpad
+from pprint import pprint
+
 
 def get_subdirs(dir):
   return [name for name in os.listdir(dir) if os.path.isdir(name)]
+
 
 def find_ppa(ppas, name):
   for ppa in ppas:
@@ -18,7 +22,19 @@ def find_ppa(ppas, name):
       return ppa
   return None
 
-def move_packages(config):
+
+def md5_for_file(filename):
+  md5 = hashlib.md5()
+  f = open(filename)
+  while True:
+    data = f.read(128)
+    if not data:
+      break
+    md5.update(data)
+  return md5.digest()
+
+
+def copy_packages(config):
     cachedir = os.path.join(os.path.expanduser("~"), ".launchpadlib", "cache")
     cwd = os.getcwd()
 
@@ -50,6 +66,7 @@ def move_packages(config):
 
     sources = from_ppa.getPublishedSources(distro_series=from_series,
                                                              status="Published")
+    failed_sources = []
     for s in sources:
       source_name = s.source_package_name
       source_version = s.source_package_version
@@ -57,7 +74,7 @@ def move_packages(config):
           and (not config.exclude_packages or not source_name in config.exclude_packages)):
         print("==========\n"
               "source_package_name:", source_name, "\n"
-              "source_package_version:", source_version, "\n")
+              "source_package_version:", source_version)
         urls = s.sourceFileUrls()
         for u in urls:
           if u.endswith(".dsc"):
@@ -65,6 +82,7 @@ def move_packages(config):
             if os.access(dir, os.F_OK):
               print("{0} already exists, skipping {1}".format(dir, source_name),
                     file=sys.stderr)
+              failed_sources.append(source_name)
               break
 
             new_version = source_version
@@ -73,12 +91,14 @@ def move_packages(config):
               if pos < 0:
                 print("{0} has not the expected version number scheme, skipping {1}".format(
                       source_version,source_name), file=sys.stderr)
+                failed_sources.append(source_name)
                 break
 
               pos2 = source_version.find(config.build_number_prefix, pos)
               if pos2 < 0:
                 print("{0} has not the expected version number scheme, skipping {1}".format(
                       source_version, source_name), file=sys.stderr)
+                failed_sources.append(source_name)
                 break
 
               pos2 = pos2 + len(config.build_number_prefix)
@@ -91,6 +111,7 @@ def move_packages(config):
               if not source_version[pos2:pos3].isdigit():
                 print("{0} has not the expected version number scheme, skipping {1}".format(
                       source_version, source_name), file=sys.stderr)
+                failed_sources.append(source_name)
                 break
 
               build_number = str(int(source_version[pos2:pos3]) + 1)
@@ -99,6 +120,7 @@ def move_packages(config):
                 new_version = new_version + "~" + config.to_series_name
               print("new source_package_version:", new_version)
 
+            print("")
             os.mkdir(dir)
             os.chdir(dir)
             print("dget -xuq {0}".format(u))
@@ -108,6 +130,7 @@ def move_packages(config):
               os.chdir(subdir)
               if (os.access(changelog, os.F_OK | os.W_OK) and 
                                                      os.path.isfile(changelog)):
+                md5_pre = md5_for_file(changelog)
                 print(("dch --newversion {0} -u medium --distribution {1}"
                        "--force-distribution \"{2}\"").format(new_version,
                                                               config.to_series_name,
@@ -118,11 +141,19 @@ def move_packages(config):
                                 "--distribution", config.to_series_name,
                                 "--force-distribution", config.changelog_message],
                                 env=os.environ)
+                md5_post = md5_for_file(changelog)
+                if md5_pre == md5_post:
+                  print("can't modify changelog of {0}".format(source_name),
+                        file=sys.stderr)
+                  failed_sources.append(source_name)
+                  break
+                  
                 print("debuild -S -sa -k{0}".format(os.environ["GPGKEY"]))
                 subprocess.call(["debuild", "-S", "-sa", "-k{0}".format(
                                                          os.environ["GPGKEY"])],
                                 preexec_fn = lambda: signal(SIGPIPE, SIG_DFL),
                                 env=os.environ)
+
               os.chdir(os.path.join(cwd, dir))
               changes_file = "{0}_{1}_source.changes".format(
                                                        source_name, new_version)
@@ -130,16 +161,22 @@ def move_packages(config):
                   not os.path.isfile(changes_file)):
                 print("can't find {0} for upload".format(changes_file),
                       file=sys.stderr)
+                failed_sources.append(source_name)
               else:
-                print("dput -U ppa: {0}/{1} {2}".format(config.to_user_name,
+                print("dput -U ppa:{0}/{1} {2}".format(config.to_user_name,
                                                         config.to_ppa_name,
                                                         changes_file))
                 subprocess.call(["dput", "-U", "ppa:{0}/{1}".format(
                                                             config.to_user_name,
                                                             config.to_ppa_name),
                                 changes_file], env=os.environ)
+
             os.chdir(cwd)
             shutil.rmtree(dir)
+
+    if failed_sources:
+      print("there were failures on the following packages:")
+      print(failed_sources, file=sys.stderr)
 
 
 class Config:
@@ -201,7 +238,8 @@ class Config:
 
 def main():
     config = Config()
-    move_packages(config)
+    pprint(vars(config))
+    copy_packages(config)
 
         
 if __name__ == '__main__':
