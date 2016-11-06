cd8375f7da3d5524bcc5a08e7edd80218c2cef61
Lars Hanisch <dvb@flensrocker.de>
2016-11-06 09:44:03 +0100

    adjust sundtek-rebuild script for yavdr 0.6

--- sundtek-rebuild.sh
+++ sundtek-rebuild.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-./yalptool.py -c sundtek-rebuild-stable-vdr.conf
-./yalptool.py -c sundtek-rebuild-stable-yavdr.conf
-./yalptool.py -c sundtek-rebuild-testing-vdr.conf
-./yalptool.py -c sundtek-rebuild-testing-yavdr.conf
+eval $(gpg-agent --daemon)
+
+./yalptool.py -c sundtek-rebuild-stable-vdr-0.5.conf
+./yalptool.py -c sundtek-rebuild-stable-yavdr-0.5.conf
+./yalptool.py -c sundtek-rebuild-testing-vdr-0.5.conf
+./yalptool.py -c sundtek-rebuild-testing-yavdr-0.5.conf
+
+./yalptool.py -c sundtek-rebuild-stable-vdr-0.6.conf
+./yalptool.py -c sundtek-rebuild-stable-yavdr-0.6.conf
+./yalptool.py -c sundtek-rebuild-testing-vdr-0.6.conf
+./yalptool.py -c sundtek-rebuild-testing-yavdr-0.6.conf
+
 ./yalptool.py -c sundtek-rebuild-unstable-vdr.conf
 ./yalptool.py -c sundtek-rebuild-unstable-yavdr.conf
 
